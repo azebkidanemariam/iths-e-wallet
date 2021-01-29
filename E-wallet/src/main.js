9f471c8c18bfced3bde7728f4dd8a9363a4d46d8
Azeb.K <54040527+azebkidanemariam@users.noreply.github.com>
2021-01-29 05:16:23 +0100

    chng

--- E-wallet/src/main.js
+++ E-wallet/src/main.js
@@ -1,7 +1,7 @@
 import Vue from "vue";
-import router from "./router";
+import router from "../src/router";
 
-import App from "./App.vue";
+import App from "../src/App.vue";
 
 Vue.config.productionTip = false;
 
